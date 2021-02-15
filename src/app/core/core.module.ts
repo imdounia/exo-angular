5914b2075f10f37bc011d76da22e632fc83f26d2
dounia <douniabakhkhouch2000@gmail.com>
2021-02-15 15:19:34 +0100

    init shared

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports : [HeaderComponent, FooterComponent],
 })
 export class CoreModule { }
